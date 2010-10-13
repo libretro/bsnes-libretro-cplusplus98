a13abab5835ebc10fea986abc53866a1e7ffc677
Themaister <maister@archlinux.us>
2010-10-13 21:27:47 +0200

    :p

--- libco/libco.h
+++ libco/libco.h
@@ -1,40 +1,37 @@
 /* Minimal cooperative (non-preemptive) threading library */
-
-/* libco 0.12 (2008-01-07) */
 #ifndef LIBCO_H
 #define LIBCO_H
 
 #include <stdint.h>
 
 #ifdef __cplusplus
-	extern "C" {
+extern "C" {
 #endif
 
+   /* Reference to a cooperative thread */
+   typedef void* cothread_t;
 
-/* Reference to a cooperative thread */
-typedef void* cothread_t;
+   /* Sets size of main stack and stack space for other threads in portable version.
+      Defaults to 64K and 256K, respectively. Must be set before first call to
+      co_create(), otherwise it has no effect. */
+   void co_setmax( unsigned main_size, unsigned total_stack );
 
-/* Sets size of main stack and stack space for other threads in portable version.
-Defaults to 64K and 256K, respectively. Must be set before first call to
-co_create(), otherwise it has no effect. */
-void co_setmax( unsigned main_size, unsigned total_stack );
+   /* Currently active thread. Before first call to co_switch(), returns main thread. */
+   cothread_t co_active( void );
 
-/* Currently active thread. Before first call to co_switch(), returns main thread. */
-cothread_t co_active( void );
+   /* Creates new thread with stack of size bytes and executes entry() when next
+      switched to. Returns pointer to thread, or NULL if it failed. User entry()
+      function must NEVER return. */ 
+   cothread_t co_create( unsigned int size, void (*entry)( void ) );
 
-/* Creates new thread with stack of size bytes and executes entry() when next
-switched to. Returns pointer to thread, or NULL if it failed. User entry()
-function must NEVER return. */ 
-cothread_t co_create( unsigned int size, void (*entry)( void ) );
+   /* Switches to thread. Thread must not already be the active one. */
+   void co_switch( cothread_t thread );
 
-/* Switches to thread. Thread must not already be the active one. */
-void co_switch( cothread_t thread );
-
-/* Deletes thread. Thread must not be the active one or main thread. */
-void co_delete( cothread_t thread );
+   /* Deletes thread. Thread must not be the active one or main thread. */
+   void co_delete( cothread_t thread );
 
 #ifdef __cplusplus
-	}
+}
 #endif
 
 #endif
