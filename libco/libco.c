e1c037e95bd4e04a4deadc8342a4f4d2c3c77a2a
Themaister <maister@archlinux.us>
2010-10-13 10:20:23 +0200

    Passed libco test, although very slowly.

--- libco/libco.c
+++ libco/libco.c
@@ -20,7 +20,7 @@ static int g_first = 1;
 
 static void allocate_first(void)
 {
-   fprintf(stderr, "main thread is %zu\n", (size_t)pthread_self());
+   //fprintf(stderr, "main thread is %zu\n", (size_t)pthread_self());
    g_current = calloc(1, sizeof(cothread_data_t));
    pthread_mutex_init(&g_current->cond_lock, NULL);
    pthread_cond_init(&g_current->cond, NULL);
@@ -35,25 +35,29 @@ static void allocate_first(void)
 
 void co_switch(cothread_t t)
 {
-   fprintf(stderr, "co_switch() in thread %zu\n", (size_t)pthread_self());
+   //fprintf(stderr, "co_switch() in thread %zu\n", (size_t)pthread_self());
 
    cothread_data_t *cur = g_current;
    g_current = t;
 
-   pthread_cond_signal(&g_current->cond);
    pthread_mutex_lock(&cur->cond_lock);
 
-   //pthread_mutex_lock(&g_current->cond_lock);
-   //g_current->can_sleep = 0;
-   //pthread_mutex_unlock(&g_current->cond_lock);
+   pthread_mutex_lock(&g_current->cond_lock);
+   g_current->can_sleep = 0;
+   pthread_cond_signal(&g_current->cond);
+   pthread_mutex_unlock(&g_current->cond_lock);
 
-   //if (cur->can_sleep)
-   //{
+   if (cur->can_sleep)
+   {
       pthread_cond_wait(&cur->cond, &cur->cond_lock);
+      cur->can_sleep = 1;
       pthread_mutex_unlock(&cur->cond_lock);
-   //}
-   //pthread_mutex_unlock(&cur->cond_lock);
-   //cur->can_sleep = 1;
+   }
+   else
+   {
+      cur->can_sleep = 1;
+      pthread_mutex_unlock(&cur->cond_lock);
+   }
    if (!cur->active)
       pthread_exit(NULL);
 }
@@ -82,11 +86,20 @@ static void* co_entry(void* in_data)
    cothread_data_t *data = in_data;
 
    pthread_mutex_lock(&data->cond_lock);
-   pthread_cond_wait(&data->cond, &data->cond_lock);
-   pthread_mutex_unlock(&data->cond_lock);
+   if (data->can_sleep)
+   {
+      pthread_cond_wait(&data->cond, &data->cond_lock);
+      data->can_sleep = 1;
+      pthread_mutex_unlock(&data->cond_lock);
+   }
+   else
+   {
+      data->can_sleep = 1;
+      pthread_mutex_unlock(&data->cond_lock);
+   }
 
    data->entry();
-   pthread_exit(NULL);
+   pthread_exit(NULL); // Can't get here. :D
 }
 
 cothread_t co_create(unsigned int heapsize, void (*entry)(void))
--- libco/libco.h
+++ libco/libco.h
@@ -10,49 +10,28 @@
 	extern "C" {
 #endif
 
-#if 0
-#if defined(__i386__) || defined(_MSC_VER)
-  #if defined(__GNUC__)
-    #if defined(__NetBSD__) || defined(__OpenBSD__)
-      #define co_abi __attribute__((regparm(2)))
-    #else
-      #define co_abi __attribute__((fastcall))
-    #endif
-  #elif defined(_MSC_VER)
-    #define co_abi __fastcall
-  #endif
-#else
-  #define co_abi
-
-#endif
-#else
-#define co_abi
-#endif
-
-
 
 /* Reference to a cooperative thread */
-//typedef struct cothread_t_* cothread_t;
 typedef void* cothread_t;
 
 /* Sets size of main stack and stack space for other threads in portable version.
 Defaults to 64K and 256K, respectively. Must be set before first call to
 co_create(), otherwise it has no effect. */
-void co_abi co_setmax( unsigned main_size, unsigned total_stack );
+void co_setmax( unsigned main_size, unsigned total_stack );
 
 /* Currently active thread. Before first call to co_switch(), returns main thread. */
-cothread_t co_abi co_active( void );
+cothread_t co_active( void );
 
 /* Creates new thread with stack of size bytes and executes entry() when next
 switched to. Returns pointer to thread, or NULL if it failed. User entry()
 function must NEVER return. */ 
-cothread_t co_abi co_create( unsigned int size, void (*entry)( void ) );
+cothread_t co_create( unsigned int size, void (*entry)( void ) );
 
 /* Switches to thread. Thread must not already be the active one. */
-void co_abi co_switch( cothread_t thread );
+void co_switch( cothread_t thread );
 
 /* Deletes thread. Thread must not be the active one or main thread. */
-void co_abi co_delete( cothread_t thread );
+void co_delete( cothread_t thread );
 
 #ifdef __cplusplus
 	}
