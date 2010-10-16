ae74611fb9cc1112ab8664ac85d8eccab3c4fc17
Themaister <maister@archlinux.us>
2010-10-16 03:31:34 +0200

    :v

--- libco/power-ucontext.h
+++ libco/power-ucontext.h
@@ -1,8 +1,12 @@
+#ifndef __UCONTEXT_H
+#define __UCONTEXT_H
+
+typedef unsigned long ulong;
+typedef unsigned int uint;
+
 #define	setcontext(u)	_setmcontext(&(u)->mc)
 #define	getcontext(u)	_getmcontext(&(u)->mc)
-typedef struct mcontext mcontext_t;
-typedef struct ucontext ucontext_t;
-struct mcontext
+typedef struct mcontext
 {
 	ulong	pc;		/* lr */
 	ulong	cr;		/* mfcr */
@@ -18,20 +22,21 @@ struct mcontext
 //	uvlong	fpr[18];	/ * callee saved: f14-f31 * /
 //	ulong	vr[4*12];	/ * callee saved: v20-v31, 256-bits each * /
 */
-};
+} mcontext_t;
 
-struct ucontext
+typedef struct ucontext
 {
 	struct {
 		void *ss_sp;
 		uint ss_size;
 	} uc_stack;
-	sigset_t uc_sigmask;
+	//sigset_t uc_sigmask;
 	mcontext_t mc;
-};
+   struct ucontext *uc_link;
+} ucontext_t;
 
 void makecontext(ucontext_t*, void(*)(void), int, ...);
 int swapcontext(ucontext_t*, const ucontext_t*);
 int _getmcontext(mcontext_t*);
 void _setmcontext(const mcontext_t*);
-
+#endif
