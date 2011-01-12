a68da8a843b7a458b39229fd9a78f6253095e5fe
Themaister <maister@archlinux.us>
2011-01-12 16:58:08 +0100

    small warning fix.

--- gameboy/interface/interface.hpp
+++ gameboy/interface/interface.hpp
@@ -5,7 +5,7 @@ public:
   virtual void video_refresh(const uint8_t *data) {}
   virtual void audio_sample(signed left, signed right) {}
   virtual void input_poll() {}
-  virtual bool input_poll(unsigned id) {}
+  virtual bool input_poll(unsigned id) { return true; }
 
   virtual void message(const string &text) { print(text, "\n"); }
 };
