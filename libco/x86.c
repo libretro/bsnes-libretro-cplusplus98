22b1452b2e512d4aa0d40cb77624de08accf0c22
Themaister <maister@archlinux.us>
2011-03-23 01:22:51 +0100

    Add some comments.

--- libco/x86.c
+++ libco/x86.c
@@ -42,18 +42,21 @@ void fastcall co_swap(cothread_t new_thread, cothread_t old_thread);
 asm (
    ".text\n"
    ASM_CO_SWAP_DEF
-   "movl %esp, (%edx)\n"
+   "movl %esp, (%edx) # Save stack pointer, and restore it again.\n"
    "movl (%ecx), %esp\n"
    "popl %eax\n"
-   "movl %ebp, 0x4(%edx)\n"
+
+   "movl %ebp, 0x4(%edx) # Save non-volatile registers to buffer.\n"
    "movl %esi, 0x8(%edx)\n"
-   "mov  %edi, 0xc(%edx)\n"
-   "mov  %ebx, 0x10(%edx)\n"
-   "mov  0x4(%ecx),  %ebp\n"
-   "mov  0x8(%ecx),  %esi\n"
-   "mov  0xc(%ecx),  %edi\n"
-   "mov  0x10(%ecx), %ebx\n"
-   "jmp  *%eax\n"
+   "movl %edi, 0xc(%edx)\n"
+   "movl %ebx, 0x10(%edx)\n"
+
+   "movl 0x4(%ecx),  %ebp # Restore non-volatile registers from buffer.\n"
+   "movl 0x8(%ecx),  %esi\n"
+   "movl 0xc(%ecx),  %edi\n"
+   "movl 0x10(%ecx), %ebx\n"
+
+   "jmp  *%eax # Jump back to saved PC.\n"
 );
 
 static void crash() {
